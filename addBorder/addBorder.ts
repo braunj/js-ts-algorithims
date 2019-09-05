244f0684c52e8754a8a48e387c856805725c9e7b
braunj <braunj@qddev.com>
2019-09-05 14:03:53 -0600

    addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,17 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[], bc: string): string[] {
+   const len: number = picture[0].length;
 
+   const bs: string = bc.repeat(len + 2);
+
+   for (let i: number = 0; i < picture.length; i++) {
+      picture[i] = bc + picture[i] + bc;
+   }
+
+   picture.unshift(bs);
+   picture.push(bs);
+
+   return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+
+console.log(addBorder(["abc", "ded", "bob"], "*"));
