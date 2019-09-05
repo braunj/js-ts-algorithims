e3fb09b7b4f2e9c0cf25fcde58740c66a1914ed9
braunj <braunj@qddev.com>
2019-09-05 15:08:50 -0600

    allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,17 @@
 function allLongestStrings(inputArray: string[]): string[] {
+   let l: number = inputArray.length;
+   let lsa: string[] = [];
+   let ls: number = 0;
 
+   inputArray.forEach((elem: string) => {
+      if (elem.length > ls) { ls = elem.length; }
+   });
+
+   for (let i: number = 0; i < l; i++) {
+      if (inputArray[i].length === ls) { lsa.push(inputArray[i]); }
+   }
+
+   return lsa;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba", "foobar"]));
