558e28233a69f132c1ee9feec5c26f048439ad48
braunj <braunj@qddev.com>
2019-09-05 14:49:58 -0600

    adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,17 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+   let b: number = Number.NEGATIVE_INFINITY;
+   let l: number = inputArray.length - 1;
 
+   for (let i: number = 0; i < l; i++) {
+      let t: number = mult(inputArray[i], inputArray[i + 1]);
+      if (t > b) { b = t; }
+   }
+
+   return b;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+function mult(a: number, b: number): number {
+   return a * b;
+}
+
+console.log(adjacentElementsProduct([-3, 6, -2, 5, -7, 3, -9]));
