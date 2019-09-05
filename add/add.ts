dd458ae79fe7d82ffaa795ce237913fc7cc06c2b
braunj <braunj@qddev.com>
2019-09-05 13:32:08 -0600

    add

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,37 @@
+// sum two numbers
 function add(param1: number, param2: number): number {
-
+   return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
+// sum unknown # of arguments as an array
+function add2(...param1: number[]): number {
+   let sum: number = 0;
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+   param1.forEach(element => {
+      sum += element;
+   });
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+   return sum;
+}
+
+// sum elements of an array
+function add3(param1: number[]): number {
+   let sum: number = 0;
+
+   param1.forEach(element => {
+      sum += element;
+   });
+
+   return sum;
+}
+
+console.log(add(1, 2));
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
+
+console.log(add3([1, 2, 3, 4, 5, 6]));
+console.log(add3([2, 3.3]));
 
