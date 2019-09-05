2fb048dd2e74e73bbc7d73cf80d8de0206649e5a
braunj <braunj@qddev.com>
2019-09-05 14:34:27 -0600

    addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
-function addTwoDigits(n: any): number {
+function addTwoDigits(n: number): number {
+   let s: string = n.toString(10);
 
+   return parseInt(s[0], 10) + parseInt(s[1], 10);
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(32));
