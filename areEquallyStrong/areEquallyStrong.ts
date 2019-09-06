dd46f47392086989f719c78530a49b07d61e3194
braunj <braunj@qddev.com>
2019-09-06 12:04:10 -0600

    areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+   let ys: number = Math.max(yourLeft, yourRight);
+   let yw: number = Math.min(yourLeft, yourRight);
 
+   let fs: number = Math.max(friendsLeft, friendsRight);
+   let fw: number = Math.min(friendsLeft, friendsRight);
+
+   return ys === fs && yw === fw;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
