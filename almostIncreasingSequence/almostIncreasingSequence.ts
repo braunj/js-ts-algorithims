fb29ca34de4c8fe1f42682d9b0b799fba466b965
braunj <braunj@qddev.com>
2019-09-06 10:09:10 -0600

    almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,17 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+   let isBadSeq: boolean = false;
+   for (let i: number = 0; i < sequence.length - 1; i++) {
+      if (sequence[i] >= sequence[i + 1]) {
+         if (isBadSeq) {
+            return !isBadSeq;
+         } else {
+            isBadSeq = !isBadSeq;
+         }
+      }
+   }
 
+   return isBadSeq;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 1, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
