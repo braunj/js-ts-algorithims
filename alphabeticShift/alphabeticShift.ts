a1722aec6a2542033177d2dea417d5f8d7d479a3
braunj <braunj@qddev.com>
2019-09-06 10:57:30 -0600

    alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,16 @@
 function alphabeticShift(inputString: string): string {
-
+   let result: string = "";
+   for (let i: number = 0; i < inputString.length; i++) {
+      let cc: number = inputString.charCodeAt(i);
+      switch (cc) {
+         case 122: result += "a";
+            break;
+         case 90: result += "A";
+            break;
+         default: result += String.fromCharCode(++cc);
+      }
+   }
+   return result;
 }
 
-console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift("CraZy"));
