f4b8a64029cf7a1cbe6b74cddef535521fa00b18
braunj <braunj@qddev.com>
2019-09-06 11:49:45 -0600

    alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,17 @@
 function alternatingSums(a: number[]): number[] {
+   let tw: number[] = [0, 0];
+   let tm: boolean = true;
 
+   a.forEach(mw => {
+      if (tm) {
+         tw[0] += mw;
+      } else {
+         tw[1] += mw;
+      }
+      tm = !tm;
+   });
+
+   return tw;
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
