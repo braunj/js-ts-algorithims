e35e8c2ecc034085b31265280d26895ed71afc44
braunj <braunj@qddev.com>
2019-09-06 11:36:06 -0600

    alphabetSubsequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,20 @@
 function alphabetSubsequence(s: string): boolean {
 
+   for (let i: number = 0; i < s.length - 1; i++) {
+      let cc: number = s.charCodeAt(i);
+      let cn: number = s.charCodeAt(i + 1);
+
+      if (cn <= cc) {
+         return false;
+      }
+   }
+
+   return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
