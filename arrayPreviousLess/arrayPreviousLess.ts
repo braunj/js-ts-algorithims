9b572c46db66e68bfdc507eb231142fa4259d763
braunj <braunj@qddev.com>
2019-09-09 15:59:29 -0600

    arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,17 @@
 function arrayPreviousLess(items: number[]): number[] {
+   let na: number[] = [-1];
 
+   for (let i: number = 1; i < items.length; i++) {
+      let t: number = -1;
+      for (let j: number = 0; j < i; j++) {
+         if (items[j] < items[i]) { t = items[j]; }
+      }
+      na.push(t);
+   }
+
+   return na;
 }
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 5, 4, 5]));
+console.log(arrayPreviousLess([3, 3, 3, 3, 3]));
