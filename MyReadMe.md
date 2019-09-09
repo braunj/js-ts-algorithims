97bcc1efb183227a76749302fff1eda52f1b85ef
braunj <braunj@qddev.com>
2019-09-09 14:39:31 -0600

    arrayChange

---
+++ MyReadMe.md
@@ -0,0 +1,7 @@
+# 100 Algorithims Challenge
+
+<https://thinkster.io/tutorials/100-algorithms-challenge>
+
+My solutions to each of the challenges which do not necessarily agree with the suggested solutions.
+
+5-9 Sept 2019
--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,20 @@
 function arrayChange(inputArray: number[]): number {
- 
+   let cnt: number = 0;
+
+   for (let i: number = 1; i < inputArray.length; i++) {
+      while (inputArray[i] <= inputArray[i - 1]) {
+         inputArray[i]++;
+         cnt++;
+      }
+   }
+
+   console.log(inputArray);
+
+   return cnt;
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 4, 7]));
+console.log(arrayChange([1, 1, 1, 7]));
+console.log(arrayChange([1, 1, 1, -7]));
+console.log(arrayChange([1, 1, 1, 2]));
