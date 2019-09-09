ec95b909ea8122591ad40b7888686901a7a6d012
braunj <braunj@qddev.com>
2019-09-09 16:07:00 -0600

    arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,16 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(inputArray: number[],
+   elemToReplace: number,
+   substitutionElem: number
+): number[] {
+   for (let i: number = 0; i < inputArray.length; i++) {
+      if (inputArray[i] === elemToReplace) {
+         inputArray[i] = substitutionElem;
+      }
+   }
+
+   return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 7, 3));
+console.log(arrayReplace([2, 1, 2], 1, 3));
