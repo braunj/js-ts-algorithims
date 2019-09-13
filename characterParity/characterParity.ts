1424128f5976e4276839212fe04dbbf9827f8719
braunj <braunj@qddev.com>
2019-09-13 15:17:34 -0600

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,16 @@
 function characterParity(symbol: string): string {
-   
+   const s: number = symbol.charCodeAt(0);
+
+   if (s < 47 || s > 57) {
+      return "not a digit";
+   } else if (s % 2) {
+      return "odd";
+   } else { return "even"; }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity("0"));
+console.log(characterParity("104"));
+console.log(characterParity("9"));
+console.log(characterParity("5"));
+console.log(characterParity("!"));
+console.log(characterParity("q"));
