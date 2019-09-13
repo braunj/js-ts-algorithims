1a1bfbf0f758bec318a9a831d8a6dead58159409
braunj <braunj@qddev.com>
2019-09-13 15:21:17 -0600

    checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,7 @@
 function checkPalindrome(inputString: string): boolean {
-
+   return inputString.toLowerCase() === inputString.toLowerCase().split("").reverse().join().replace(/,/g, '');
 }
 
-console.log(checkPalindrome('aabaa'));
-console.log(checkPalindrome('abac'));
-console.log(checkPalindrome('a'));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
