8cec7c634f96c00aaef6e0d996491b8aee38e7a5
braunj <braunj@qddev.com>
2019-09-13 14:30:44 -0600

    candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,10 @@
-function candies(n: number, m: number): number {
-
+function candies(children: number, candy: number): number {
+   return Math.floor(candy / children) * children;
 }
 
 console.log(candies(3, 10));
+console.log(candies(4, 10));
+console.log(candies(5, 10));
+console.log(candies(10, 9));
+console.log(candies(10, 10));
+console.log(candies(10, 11));
