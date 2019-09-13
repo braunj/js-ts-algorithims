ad35faf893c9408fc96a6a0bfb98040ad17a8970
braunj <braunj@qddev.com>
2019-09-13 17:34:38 -0600

    return sum / count || 0;

--- companyBotStrategy/companyBotStrategy.ts
+++ companyBotStrategy/companyBotStrategy.ts
@@ -1,21 +1,17 @@
-function companyBotStrategy(td: number[][]): number {
+function companyBotStrategy(trainingData: number[][]): number {
 
    let sum: number = 0;
    let count: number = 0;
 
-   for (let a: number = 0; a < td.length; a++) {
-      if (td[a][1] === 1) {
+   for (let a: number = 0; a < trainingData.length; a++) {
+      if (trainingData[a][1] === 1) {
          count++;
-         sum += td[a][0];
+         sum += trainingData[a][0];
          // console.log(td[a]);
       }
    }
 
-   if (count) {
-      return sum / count;
-   }
-
-   return count;
+   return sum / count || 0;
 }
 
 console.log(companyBotStrategy([[3, 1], [6, 1], [4, 1], [5, 1]]));
