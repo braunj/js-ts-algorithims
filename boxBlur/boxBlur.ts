12817629db24340a7493e46e7177ef5875721c22
braunj <braunj@qddev.com>
2019-09-13 11:59:12 -0600

    boxBlur 2

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -4,23 +4,20 @@ function boxBlur(image: number[][]): number[][] {
    let blur: number[][] = [];
    let smBox: number[] = [];
 
+   for (let row: number = 0; row <= sz - bx; row++) {
+      blur.push(smBox);
 
+      for (let col: number = 0; col <= sz - bx; col++) {
 
-   let p: number = 0;
-
-   for (let col: number = 0; col <= sz - bx; col++) {
-      for (let row: number = 0; row <= sz - bx; row++) {
-         blur.push(smBox);
          let sum: number = 0;
+
          for (let r: number = 0; r < bx; r++) {
             for (let c: number = 0; c < bx; c++) {
                sum += image[col + c][row + r];
             }
          }
 
-         p = Math.floor(sum / 9);
-         console.log(p);
-         blur[row][col] = p;
+         blur[row][col] = Math.floor(sum / (bx * bx));
       }
    }
 
