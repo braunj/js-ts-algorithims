2b6022a53862bd912f5ed82207b0c0b53bda8e0c
braunj <braunj@qddev.com>
2019-09-13 15:53:59 -0600

    chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,20 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
+   let foo: any[] = [];
 
+   for (let i: number = 0; i < arr.length; i += size) {
+      let moo: any[] = [];
+      for (let j: number = 0; j < size; j++) {
+         if (i + j >= arr.length) { break; }
+
+         moo.push(arr[i + j]);
+      }
+      foo.push(moo);
+   }
+   return foo;
 }
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["foo", "moo", "boo", "zoo"], 1));
+console.log(chunkyMonkey(["foo", "moo", 2, 4, 6, 8, "boo", "zoo"], 12));
+console.log(chunkyMonkey(["foo", "moo", 2, 4, 6, 8, "boo", "zoo"], 3));
