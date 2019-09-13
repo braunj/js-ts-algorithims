374e4680e317fe3fae2edfee87dbadae623e24b0
braunj <braunj@qddev.com>
2019-09-13 16:12:57 -0600

    circleOfNumbers

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,11 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
 
+   return ((n / 2) + firstNumber) % n;
 }
 
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 3));
+console.log(circleOfNumbers(10, 6));
+console.log(circleOfNumbers(12, 2));
+console.log(circleOfNumbers(12, 8));
+console.log(circleOfNumbers(12, 6));
