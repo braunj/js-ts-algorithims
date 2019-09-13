61281a74ef87eb9e3cb24a42b507f7635c53e607
braunj <braunj@qddev.com>
2019-09-13 14:48:23 -0600

    isCaseInsensitivePalindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,8 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+   return inputString.toLowerCase() === inputString.toLowerCase().split("").reverse().join().replace(/,/g, '');
 }
 
-console.log(isCaseInsensitivePalindrome('AaBaa'));
-console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome("AaBaa"));
+console.log(isCaseInsensitivePalindrome("abac"));
+console.log(isCaseInsensitivePalindrome("a"));
+console.log(isCaseInsensitivePalindrome(""));
