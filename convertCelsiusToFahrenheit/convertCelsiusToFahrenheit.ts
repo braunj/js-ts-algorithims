a42b4b26bea122f401ee97574265671a949ce275
braunj <braunj@qddev.com>
2019-09-16 12:10:24 -0600

    celsiusToFahrenheit

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,7 +1,19 @@
 function celsiusToFahrenheit(celsius: number): number {
+   return c2f(celsius);
+}
 
+function c2f(c: number): number {
+   return ((9 * c) / 5) + 32;
+}
+
+function f2c(f: number): number {
+   return (f - 32) * 5 / 9;
 }
 
 console.log(celsiusToFahrenheit(-30));
 console.log(celsiusToFahrenheit(-10));
 console.log(celsiusToFahrenheit(0));
+
+console.log(f2c(-22));
+console.log(f2c(14));
+console.log(f2c(32));
