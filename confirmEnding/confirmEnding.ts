47c5ef41534b17c4970ff0d2a76bca5a4ece081c
braunj <braunj@qddev.com>
2019-09-16 09:08:52 -0600

    confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,9 @@
-function confirmEnding(str: string, target: string) {
-   
+function confirmEnding(str: string, target: string): boolean {
+   return str.lastIndexOf(target) === str.length - target.length;
 }
 
 console.log(confirmEnding("Abstraction", "action"));
 console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding("Open sesame", ""));
+console.log(confirmEnding("Open sesame", "e"));
+console.log(confirmEnding("Open sesame", "ee"));
