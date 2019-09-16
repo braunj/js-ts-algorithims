9d18b91c408c0bcdf4e880d456bdaff604024517
braunj <braunj@qddev.com>
2019-09-16 12:01:48 -0600

    containsDuplicates

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,10 @@
 function containsDuplicates(a: number[]): boolean {
+   let set: Set<number> = new Set(a);
 
+   return !(set.size === a.length);
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
-console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([3, 2, 1]));
+console.log(containsDuplicates([]));
+console.log(containsDuplicates([3, 1, 3]));
