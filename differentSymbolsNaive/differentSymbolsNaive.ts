e7fcefa5be4aeea6ae2c134b799f2eb78dc002fa
braunj <braunj@qddev.com>
2019-09-26 11:18:40 -0600

    differentSymbolsNaive

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,10 @@
 function differentSymbolsNaive(s: string): number {
- 
+
+   let ss: Set<string> = new Set(s.split(""));
+
+   return ss.size;
 }
 
-console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive(""));
+console.log(differentSymbolsNaive("cabca"));
+console.log(differentSymbolsNaive("abcde"));
