8bda29f6952f8c31edd2661801b0e0fbfb900b6a
braunj <braunj@qddev.com>
2019-09-26 10:57:41 -0600

    crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,31 @@
-function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+function crossingSum(matrix: number[][], row: number, col: number): number {
+   let foo: number = matrix[row].reduce(reducer);
+   foo += colsum(matrix, row, col);
+   return foo;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+const reducer: any = (accumulator, currentValue) => accumulator + currentValue;
+
+const colsum: any = (m: number[][], r: number, c: number) => {
+   let sum: number = 0;
+
+   for (let i: number = 0; i < m.length; i++) {
+      if (i !== r) {
+         sum += m[i][c];
+      }
+   }
+
+   return sum;
+};
+
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 1, 3));
+
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 0, 0));
+
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 2, 3));
