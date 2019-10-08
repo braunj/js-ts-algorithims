9ccb17eba539650d38147d877f86b1876824ca05
braunj <braunj@qddev.com>
2019-10-08 04:18:16 -0600

    domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,20 @@
-function domainType(domains: string[]): string[] {
 
+function domainType(domains: string[]): string[] {
+   let list: string[] = [];
+
+   domains.forEach(sub => {
+      let domain: string = sub.substring(sub.lastIndexOf("."));
+      switch (domain) {
+         // "organization", "commercial", "network", "information"
+         case ".com": list.push("commercial"); break;
+         case ".org": list.push("organization"); break;
+         case ".net": list.push("network"); break;
+         case ".info": list.push("information"); break;
+         default: list.push("unknown");
+      }
+   });
+
+   return list;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(domainType(["en.wiki.org", "cb77.org", "codefights.com", "happy.net", "code.info"]));
