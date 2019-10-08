20d2c94bb30773e4fae792cf174c896d41ce0112
braunj <braunj@qddev.com>
2019-10-08 11:49:58 -0600

    evenDigitsOnly

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,13 @@
 function evenDigitsOnly(n: number): boolean {
+   let ns: number[] = [...n + ""].map(Number);
 
+   for (let i: number = 0; i < ns.length; i++) {
+      if (ns[i] % 2 !== 0) {
+         return false;
+      }
+   }
+
+   return true;
 }
 
 console.log(evenDigitsOnly(248622));
