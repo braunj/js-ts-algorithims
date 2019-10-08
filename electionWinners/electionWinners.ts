c3e41ea257ecf898ccc745ae60bf072280443789
braunj <braunj@qddev.com>
2019-10-08 04:56:43 -0600

    electionsWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,14 @@
 function electionsWinners(votes: number[], k: number): number {
+   const max: number = Math.max(...votes);
 
+   return votes.reduce((tot, curr) => {
+      if (curr + k > max) {
+         tot++;
+      }
+      return tot;
+   }, 0);
 }
 
+console.log(electionsWinners([2, 3, 5, 2], 1));
+console.log(electionsWinners([2, 3, 5, 2], 2));
 console.log(electionsWinners([2, 3, 5, 2], 3));
