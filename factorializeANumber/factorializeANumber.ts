d2f3c994b93e43c3c6208e0b939127e4bd207abf
braunj <braunj@qddev.com>
2019-10-08 12:28:54 -0600

    factorializeANumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,11 @@
 function factorializeANumber(num: number): number {
-
+   let f: number = 1;
+   for (let i: number = 1; i <= num; i++) {
+      f *= i;
+   }
+   return f;
 }
 
+console.log(factorializeANumber(0));
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
