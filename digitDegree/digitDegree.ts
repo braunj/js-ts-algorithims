4c045696ce8eca2053372e8261513f391ba9afee
braunj <braunj@qddev.com>
2019-10-08 03:54:26 -0600

    digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,22 @@
 function digitDegree(n: number): number {
-  
+   let counter: number = 0;
+
+   while (n > 9) {
+      let x: number = n.toString()
+         .split("")
+         .map(Number)
+         .reduce((acc, curValue) => { return acc + curValue; });
+
+      n = x;
+      counter++;
+   }
+
+   return counter;
 }
 
+console.log(digitDegree(0));
 console.log(digitDegree(5));
 console.log(digitDegree(10));
 console.log(digitDegree(91));
+console.log(digitDegree(191));
+console.log(digitDegree(99191));
