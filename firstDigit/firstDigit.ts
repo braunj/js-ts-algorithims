4cedeae67ea3ece83350d95644ca6e359ddf070e
braunj <braunj@qddev.com>
2019-10-08 20:59:21 -0600

    firstDigit

--- firstDigit/firstDigit.ts
+++ firstDigit/firstDigit.ts
@@ -1,7 +1,14 @@
 function firstDigit(inputString: string): string {
+   for (let i: number = 0; i < inputString.length; i++) {
+      if (!isNaN(parseInt(inputString.charAt(i), 10))) {
+         return inputString.charAt(i);
+      }
+   }
 
+   return "no digits";
 }
 
 console.log(firstDigit('var_1__Int'));
 console.log(firstDigit('q2q-q'));
 console.log(firstDigit('0ss'));
+console.log(firstDigit('Oss'));
