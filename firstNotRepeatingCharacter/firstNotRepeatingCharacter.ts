b5d8bddaab710add981dbd4abc81980fd9c07c77
braunj <braunj@qddev.com>
2019-10-09 16:34:53 -0600

    firstNotRepeatingCharacter

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,28 @@
 function firstNotRepeatingCharacter(s: string): string {
+   let map: Map<string, number> = new Map();
+   let result: string = "\'_\'";
 
+   for (let i: number = 0; i < s.length; i++) {
+      let c: string = s.charAt(i);
+      if (map.has(c)) {
+         map.set(c, map.get(c) + 1);
+      } else {
+         map.set(c, 1);
+      }
+   }
+
+   let first: boolean = true;
+   map.forEach((v, k) => {
+      if (v === 1 && first) {
+         result = k;
+         first = !first;
+      }
+   });
+
+   return result;
 }
+
+
 
 console.log(firstNotRepeatingCharacter('abacabad'));
 console.log(firstNotRepeatingCharacter('abacabaabacaba'));
