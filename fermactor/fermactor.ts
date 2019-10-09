f5ce1c91955c70e10b5bab7c31c9b25e015f5fa6
braunj <braunj@qddev.com>
2019-10-08 18:57:36 -0600

    fermactor

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,15 @@
 function fermactor(n: number): number[] {
+   let a: number = Math.ceil(Math.sqrt(n));
+   let b: number;
 
+   do {
+      b = Math.sqrt(a * a++ - n);
+   } while (b % 1 !== 0);
+
+   return [--a, b, a * a, b * b, a * a - b * b];
 }
 
 console.log(fermactor(15));
+console.log(fermactor(5959));
+console.log(fermactor(151));
+console.log(fermactor(1515));
