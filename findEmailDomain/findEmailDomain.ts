7d9b2ddcbcb24172f1242c8653e5fc5aa112bb6f
braunj <braunj@qddev.com>
2019-10-08 19:33:29 -0600

    findEmailDomain

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,8 @@
 function findEmailDomain(address: string): string {
-
+   return address.substring(address.lastIndexOf("@") + 1);
 }
 
-console.log(findEmailDomain('prettyandsimple@example.com'));
-console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain("prettyandsimple@example.com"));
+console.log(findEmailDomain("<>[]:,;@\\'!#$%&*+-/=?^_{}| ~.a\\'@example.org"));
+console.log(findEmailDomain("braunj@cb77.org"));
+console.log(findEmailDomain("james.braun@optum.com"));
